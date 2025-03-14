b5647f8b7325cc18b99051dd23eca28d929214ec
Mafmerlos <matheusmerlos02@gmail.com>
2025-03-14 15:01:11 -0300

    Many-to-many association with JoiTable

--- src/main/java/com/aprendendoSpring/project/entities/Product.java
+++ src/main/java/com/aprendendoSpring/project/entities/Product.java
@@ -9,36 +9,38 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
-	private static final long serialVersionUID = 1L;  
-
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	
-	private Set<Category> categories = new HashSet<>(); //Nao colocar coleção no construtor 
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>(); // Nao colocar coleção no construtor
 
 	public Product() {
 	}
-	
+
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUrl = imgUrl;		//Coleção ja instanciada na parte de cima
+		this.imgUrl = imgUrl; // Coleção ja instanciada na parte de cima
 	}
 
 	public Long getId() {
@@ -82,7 +84,7 @@ public class Product implements Serializable{
 	}
 
 	public Set<Category> getCategories() {
-		return categories;    //Apenas metodo GET para coleções
+		return categories; // Apenas metodo GET para coleções
 	}
 
 	@Override
@@ -101,6 +103,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
 
 }
